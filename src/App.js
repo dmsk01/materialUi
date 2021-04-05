795d6ab203fbd2f3f049f957d2271d7139b3144a
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-04-05 20:38:44 +0300

    footer, log in and register form added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import { AppBar, Container, Toolbar, IconButton, Typography, Box, Grid, Card, CardMedia, CardContent, CardActions } from "@material-ui/core";
+import React from "react";
+import { AppBar, Container, Toolbar, IconButton, Typography, Box, Grid, Card, CardMedia, CardContent, CardActions, BottomNavigation, BottomNavigationAction, Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions } from "@material-ui/core";
+
 import Button from "@material-ui/core/Button";
+
 import MenuIcon from "@material-ui/icons/Menu";
 import LayerIcon from "@material-ui/icons/Layers";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FolderIcon from "@material-ui/icons/Folder";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+
 import Paper from "@material-ui/core/Paper";
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -12,6 +20,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    marginTop: theme.spacing(4),
   },
   menuButton: {
     marginRight: theme.spacing(1),
@@ -43,6 +52,9 @@ const useStyles = makeStyles((theme) => ({
     right: 0,
     background: "rgba(0,0,0, 0.3)",
   },
+  card: {
+    cursor: "pointer",
+  },
   cardMedia: {
     paddingTop: "55%",
   },
@@ -52,12 +64,37 @@ const useStyles = makeStyles((theme) => ({
   cardGrid: {
     marginTop: theme.spacing(4),
   },
+  footerTop: {
+    marginTop: "30px",
+    marginBottom: "30px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  letUsKnowBtn: {
+    marginLeft: theme.spacing(4),
+  },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function App() {
   const classes = useStyles();
+  const [value, setValue] = React.useState("recents");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <AppBar position="fixed">
@@ -70,13 +107,48 @@ function App() {
               MaterialUI Web Site
             </Typography>
             <Box mr={3}>
-              <Button color="inherit" variant="outlined">
+              <Button onClick={handleClickOpen} color="inherit" variant="outlined">
                 Log in
               </Button>
+              <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-login">
+                <DialogTitle id="form-dialog-login">Sign in</DialogTitle>
+                <DialogContent>
+                  <DialogContentText>Log in to read more</DialogContentText>
+                  <TextField autoFocus margin="dense" id="name" label="Email" type="email" fullWidth />
+                  <TextField margin="dense" id="pass" label="Password" type="password" fullWidth />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    Cancel
+                  </Button>
+                  <Button onClick={handleClose} color="primary">
+                    Log in
+                  </Button>
+                </DialogActions>
+              </Dialog>
             </Box>
-            <Button color="secondary" variant="contained">
-              Sign up
-            </Button>
+            <Box>
+              <Button onClick={handleClickOpen} color="secondary" variant="contained">
+                Sign up
+              </Button>
+              <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-login">
+                <DialogTitle id="form-dialog-login">Register</DialogTitle>
+                <DialogContent>
+                  <DialogContentText>Register to read more</DialogContentText>
+                  <TextField autoFocus margin="dense" id="name" label="Nickname" type="name" fullWidth />
+                  <TextField margin="dense" id="mail" label="Email" type="email" fullWidth />
+                  <TextField margin="dense" id="pass" label="Password" type="password" fullWidth />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    Cancel
+                  </Button>
+                  <Button onClick={handleClose} color="primary">
+                    Sign up
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </Box>
           </Toolbar>
         </Container>
       </AppBar>
@@ -156,6 +228,20 @@ function App() {
           </Container>
         </div>
       </main>
+      <footer gutterBottom>
+        <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
+          <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
+          <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+        </BottomNavigation>
+        <div className={classes.footerTop}>
+          <Typography>Are you using Material-UI?</Typography>
+          <Button className={classes.letUsKnowBtn} variant="contained" size="small" color="secondary">
+            Let us know!
+          </Button>
+        </div>
+      </footer>
     </>
   );
 }
